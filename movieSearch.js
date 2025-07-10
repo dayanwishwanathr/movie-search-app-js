aeaae76a1cc47039f03c81fdbafa03815ef2d017
Dayan Wishwanath <68376218+dayanwishwanathr@users.noreply.github.com>
2025-07-10 19:40:43 +0530

    Fix frontend and backend

--- movieSearch.js
+++ movieSearch.js
@@ -1,3 +1,13 @@
 let ombdKey = "386e4e7d"
 
 console.log("Hello")
+
+function searchMovie() {
+    console.log("Searching...")
+
+    let inputTag = document.getElementById("input");
+    let inputValues = inputTag.value;
+
+    console.log("inputValues");
+
+}
